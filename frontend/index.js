a9f9b25fc127102acb39c85d9e07261120de1438
Ron Ofir <ronofir97@gmail.com>
2022-05-04 21:54:47 +0300

    update

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+
+// #region Add-bug event listener section
+var btnAddBug = document.getElementById("add-bug-btn");
+btnAddBug.addEventListener('click', AddBugEventListener());
+function AddBugEventListener()
+{
+    
+}
+// #endregion
