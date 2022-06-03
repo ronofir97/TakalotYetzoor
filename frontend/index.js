9801fa40bead62635eb34e9d185c389cba3aa119
Ron Ofir <48864890+ronofir97@users.noreply.github.com>
2022-06-03 13:53:20 +0300

    Delete frontend directory

--- frontend/index.js
+++
@@ -1,9 +0,0 @@
-
-// #region Add-bug event listener section
-var btnAddBug = document.getElementById("add-bug-btn");
-btnAddBug.addEventListener('click', AddBugEventListener());
-function AddBugEventListener()
-{
-    
-}
-// #endregion
